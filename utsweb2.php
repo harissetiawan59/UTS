04202ae3e4cfd7e22060654cc600396e98cb00d8
harissetiawan59 <62023113+harissetiawan59@users.noreply.github.com>
2020-04-24 23:04:46 +0700

    Add files via upload

---
+++ utsweb2.php
@@ -0,0 +1,24 @@
+<?php
+echo "<center>";
+echo "<h1>Data Pemantauan Covid 19 Wilayah Semarang</h1>";
+echo "<h2> Per Hari Ini ".date('l, d-M-Y / H:i:s a');
+echo "</tr>";
+echo "<table border =4 width =600 align = center>";
+
+echo "</tr>";
+echo "<td>Positif</td>";
+echo "<td>Dirawat</td>";
+echo "<td>Sembuh</td>";
+echo "<td>Meninggal</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>222</td>";
+echo "<td>159</td>";
+echo "<td>27</td>";
+echo "<td>36</td>";
+echo "</tr>";
+echo "<tr>";
+
+echo "</table>";
+echo "</center>";
+?>
